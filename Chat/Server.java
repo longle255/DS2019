04ad1e1c8ed9f28ba3f84bff3d144d131f465305
Long Le <hoanglong25588@gmail.com>
2019-09-29 13:35:27 +0200

    Excercise 1.3 Thread support

--- Chat/Server.java
+++ Chat/Server.java
@@ -1,40 +1,26 @@
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    private Socket socket;
+
     private ServerSocket server;
+    private int clientCount = 0;
 
     public Server(int port) {
         try {
             server = new ServerSocket(port);
             System.out.println("Server started");
             System.out.println("Waiting for a client ...");
-            socket = server.accept();
-            System.out.println("Client accepted");
 
-            // takes input from the client socket
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            String msg = "";
-
-            // reads message from client until "Over" is sent
-            while (!msg.equals("end")) {
-                try {
-                    msg = in.readUTF();
-                    System.out.println(msg);
-                    out.writeUTF(msg);
-
-                } catch (IOException i) {
-                    System.out.println(i);
-                }
+            while (true) {
+                Socket socket = server.accept();
+                System.out.println("Client accepted");
+                Thread handler = new SocketHandler(socket, ++clientCount);
+                handler.start();
             }
-            System.out.println("Closing connection");
-            // close connection
-            socket.close();
-            in.close();
-            out.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -48,4 +34,46 @@ public class Server {
         }
         Server server = new Server(Integer.parseInt(args[0]));
     }
+
+    class SocketHandler extends Thread {
+        private DataInputStream in;
+        private DataOutputStream out;
+        private Socket socket;
+        private String prefix;
+        private int clientId;
+
+        public SocketHandler(Socket socket, int clientId) throws IOException {
+            this.socket = socket;
+            this.in = new DataInputStream(socket.getInputStream());
+            this.out = new DataOutputStream(socket.getOutputStream());
+            this.clientId = clientId;
+            this.prefix = "Client[" + clientId + "]:";
+        }
+
+        @Override
+        public void run() {
+            try {
+                String msg = "";
+                while (!msg.equals("end")) {
+                    try {
+                        msg = in.readUTF();
+                        System.out.println(prefix + msg);
+                        out.writeUTF(msg);
+
+                    } catch (IOException i) {
+                        System.out.println(i);
+                    }
+                }
+                System.out.println("Closing connection");
+                // close connection
+                socket.close();
+                in.close();
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
